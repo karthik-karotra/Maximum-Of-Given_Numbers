77503c2ff8b1d7efa065fe9c5fb53fe1aa4ab65f
karthik-karotra <karthikpatel54@gmail.com>
2020-02-28 17:51:19 +0530

    ADDED:Finding maximum string from given 3 strings

--- src/main/java/com/maximumproblem/MaximumOfGivenNumbers.java
+++ src/main/java/com/maximumproblem/MaximumOfGivenNumbers.java
@@ -23,4 +23,13 @@ public class MaximumOfGivenNumbers {
         }
         return thirdNumber;
     }
+
+    public static String checkMaximum(String firstString, String secondString, String thirdString) {
+        if (firstString.compareTo(secondString) > 0 && firstString.compareTo(thirdString) > 0) {
+            return firstString;
+        } else if (secondString.compareTo(thirdString) > 0) {
+            return secondString;
+        }
+        return thirdString;
+    }
 }
--- src/test/java/com/maximumproblem/TestMaximumOfGivenNumbers.java
+++ src/test/java/com/maximumproblem/TestMaximumOfGivenNumbers.java
@@ -6,17 +6,17 @@ public class TestMaximumOfGivenNumbers {
 
     @Test
     public void givenIntegers_WhenMaximumAtFirstPosition_ShouldReturnSameNumber() {
-        Assert.assertEquals((Integer)12, MaximumOfGivenNumbers.checkMaximum(12, 10, 3 ));
+        Assert.assertSame(12, MaximumOfGivenNumbers.checkMaximum(12, 10, 3 ));
     }
 
     @Test
     public void givenIntegers_WhenMaximumAtSecondPosition_ShouldReturnSecondNumber() {
-        Assert.assertEquals((Integer)12, MaximumOfGivenNumbers.checkMaximum(10, 12, 3 ));
+        Assert.assertSame(12, MaximumOfGivenNumbers.checkMaximum(10, 12, 3 ));
     }
 
     @Test
     public void givenIntegers_WhenMaximumAtThirdPosition_ShouldReturnThirdNumber() {
-        Assert.assertEquals((Integer)12, MaximumOfGivenNumbers.checkMaximum(3, 10, 12 ));
+        Assert.assertSame(12, MaximumOfGivenNumbers.checkMaximum(3, 10, 12 ));
     }
 
     @Test
@@ -32,5 +32,20 @@ public class TestMaximumOfGivenNumbers {
     @Test
     public void givenFloatingIntegers_WhenMaximumAtThirdPosition_ShouldReturnThirdNumber() {
         Assert.assertEquals((Float)12.5f, MaximumOfGivenNumbers.checkMaximum(3.4f, 10.1f, 12.5f ));
+    }
+
+    @Test
+    public void givenStrings_WhenMaximumAtFirstPosition_ShouldReturnSameString() {
+        Assert.assertEquals((String)"Peach", MaximumOfGivenNumbers.checkMaximum("Peach","Apple","Banana"));
+    }
+
+    @Test
+    public void givenStrings_WhenMaximumAtSecondPosition_ShouldReturnSecondString() {
+        Assert.assertEquals((String) "Peach", MaximumOfGivenNumbers.checkMaximum("Apple","Peach","Banana" ));
+    }
+
+    @Test
+    public void givenStrings_WhenMaximumAtThirdPosition_ShouldReturnThirdString() {
+        Assert.assertEquals((String) "Peach", MaximumOfGivenNumbers.checkMaximum("Apple", "Banana", "Peach"));
     }
 }
