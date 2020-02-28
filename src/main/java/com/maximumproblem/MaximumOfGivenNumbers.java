75156b64a3aaa6d073b3c86c7fe8e9f43d2a2724
karthik-karotra <karthikpatel54@gmail.com>
2020-02-28 17:35:56 +0530

    ADDED:Finding maximum numbers from given 3 floating numbers

--- src/main/java/com/maximumproblem/MaximumOfGivenNumbers.java
+++ src/main/java/com/maximumproblem/MaximumOfGivenNumbers.java
@@ -1,15 +1,26 @@
 package com.maximumproblem;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class MaximumOfGivenNumbers {
+
     public static Integer checkMaximum(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
-        List arraylist=new ArrayList();
-        arraylist.add(firstNumber);
-        arraylist.add(secondNumber);
-        arraylist.add(thirdNumber);
-        Collections.sort(arraylist, Collections.reverseOrder());
-        return (Integer)arraylist.get(0);
+        if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNumber) > 0) {
+            return firstNumber;
+        } else if (secondNumber.compareTo(thirdNumber) > 0) {
+            return secondNumber;
+        }
+        return thirdNumber;
+    }
+
+    public static Float checkMaximum(Float firstNumber, Float secondNumber, Float thirdNumber) {
+        if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNumber) > 0) {
+            return firstNumber;
+        } else if (secondNumber.compareTo(thirdNumber) > 0) {
+            return secondNumber;
+        }
+        return thirdNumber;
     }
 }
--- src/test/java/com/maximumproblem/TestMaximumOfGivenNumbers.java
+++ src/test/java/com/maximumproblem/TestMaximumOfGivenNumbers.java
@@ -18,4 +18,19 @@ public class TestMaximumOfGivenNumbers {
     public void givenIntegers_WhenMaximumAtThirdPosition_ShouldReturnThirdNumber() {
         Assert.assertEquals((Integer)12, MaximumOfGivenNumbers.checkMaximum(3, 10, 12 ));
     }
+
+    @Test
+    public void givenFloatingIntegers_WhenMaximumAtFirstPosition_ShouldReturnSameNumber() {
+        Assert.assertEquals((Float)12.1f, MaximumOfGivenNumbers.checkMaximum(12.1f, 10.3f, 3.2f ));
+    }
+
+    @Test
+    public void givenFloatingIntegers_WhenMaximumAtSecondPosition_ShouldReturnSecondNumber() {
+        Assert.assertEquals((Float)12.12f, MaximumOfGivenNumbers.checkMaximum(10.5f, 12.12f, 3.4f ));
+    }
+
+    @Test
+    public void givenFloatingIntegers_WhenMaximumAtThirdPosition_ShouldReturnThirdNumber() {
+        Assert.assertEquals((Float)12.5f, MaximumOfGivenNumbers.checkMaximum(3.4f, 10.1f, 12.5f ));
+    }
 }
