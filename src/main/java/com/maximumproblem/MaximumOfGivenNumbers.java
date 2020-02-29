ea3c84737f2e6a37ec48e915db63f7bbc93539ae
karthik-karotra <karthikpatel54@gmail.com>
2020-02-29 11:54:26 +0530

    REFACTORED:Maximum function to take multiple values

--- src/main/java/com/maximumproblem/MaximumOfGivenNumbers.java
+++ src/main/java/com/maximumproblem/MaximumOfGivenNumbers.java
@@ -1,10 +1,10 @@
 package com.maximumproblem;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
-public class MaximumOfGivenNumbers <E extends Comparable>{
+import java.util.Arrays;
+import java.util.Collections;
+
+public class MaximumOfGivenNumbers<E extends Comparable> {
     E firstValueToCompare;
     E secondValueToCompare;
     E thirdValueToCompare;
@@ -16,16 +16,25 @@ public class MaximumOfGivenNumbers <E extends Comparable>{
     }
 
     public <E extends Comparable> E checkMaximum() {
-        return (E) checkMaximum(firstValueToCompare,secondValueToCompare,thirdValueToCompare);
+        return (E) checkMaximum(firstValueToCompare, secondValueToCompare, thirdValueToCompare);
     }
 
-    public static<E extends Comparable> E checkMaximum(E firstValueToCompare, E secondValueToCompare, E thirdValueToCompare) {
+    public static <E extends Comparable> E checkMaximum(E firstValueToCompare, E secondValueToCompare, E thirdValueToCompare, E... extraParameters) {
+        E maximumValue;
+        E arrayMaximumValue = null;
         if (firstValueToCompare.compareTo(secondValueToCompare) > 0 && firstValueToCompare.compareTo(thirdValueToCompare) > 0) {
-            return firstValueToCompare;
+            maximumValue = firstValueToCompare;
         } else if (secondValueToCompare.compareTo(thirdValueToCompare) > 0) {
-            return secondValueToCompare;
+            maximumValue = secondValueToCompare;
+        } else
+            maximumValue = thirdValueToCompare;
+        if (extraParameters.length != 0) {
+            Arrays.sort(extraParameters, Collections.reverseOrder());
+            if (maximumValue.compareTo(extraParameters[0]) < 0)
+                maximumValue = extraParameters[0];
         }
-        return thirdValueToCompare;
+        return maximumValue;
+
     }
 
 
--- src/test/java/com/maximumproblem/TestMaximumOfGivenNumbers.java
+++ src/test/java/com/maximumproblem/TestMaximumOfGivenNumbers.java
@@ -21,31 +21,46 @@ public class TestMaximumOfGivenNumbers {
 
     @Test
     public void givenFloatingIntegers_WhenMaximumAtFirstPosition_ShouldReturnSameNumber() {
-        Assert.assertEquals((Float)12.1f, MaximumOfGivenNumbers.checkMaximum(12.1f, 10.3f, 3.2f ));
+        Assert.assertEquals(12.1f, MaximumOfGivenNumbers.checkMaximum(12.1f, 10.3f, 3.2f ),0.0f);
     }
 
     @Test
     public void givenFloatingIntegers_WhenMaximumAtSecondPosition_ShouldReturnSecondNumber() {
-        Assert.assertEquals((Float)12.12f, MaximumOfGivenNumbers.checkMaximum(10.5f, 12.12f, 3.4f ));
+        Assert.assertEquals(12.12f, MaximumOfGivenNumbers.checkMaximum(10.5f, 12.12f, 3.4f ),0.0f);
     }
 
     @Test
     public void givenFloatingIntegers_WhenMaximumAtThirdPosition_ShouldReturnThirdNumber() {
-        Assert.assertEquals((Float)12.5f, MaximumOfGivenNumbers.checkMaximum(3.4f, 10.1f, 12.5f ));
+        Assert.assertEquals(12.5f, MaximumOfGivenNumbers.checkMaximum(3.4f, 10.1f, 12.5f ),0.0f);
     }
 
     @Test
     public void givenStrings_WhenMaximumAtFirstPosition_ShouldReturnSameString() {
-        Assert.assertEquals((String)"Peach", MaximumOfGivenNumbers.checkMaximum("Peach","Apple","Banana"));
+        Assert.assertEquals("Peach", MaximumOfGivenNumbers.checkMaximum("Peach","Apple","Banana"));
     }
 
     @Test
     public void givenStrings_WhenMaximumAtSecondPosition_ShouldReturnSecondString() {
-        Assert.assertEquals((String) "Peach", MaximumOfGivenNumbers.checkMaximum("Apple","Peach","Banana" ));
+        Assert.assertEquals("Peach", MaximumOfGivenNumbers.checkMaximum("Apple","Peach","Banana" ));
     }
 
     @Test
     public void givenStrings_WhenMaximumAtThirdPosition_ShouldReturnThirdString() {
-        Assert.assertEquals((String) "Peach", MaximumOfGivenNumbers.checkMaximum("Apple", "Banana", "Peach"));
+        Assert.assertEquals("Peach", MaximumOfGivenNumbers.checkMaximum("Apple", "Banana", "Peach"));
+    }
+
+    @Test
+    public void givenMultipleIntegers_ShouldReturnMaximumValue() {
+        Assert.assertSame(12, MaximumOfGivenNumbers.checkMaximum(12, 10, 3,4,9,7 ));
+    }
+
+    @Test
+    public void givenMultipleFloatingNumbers_ShouldReturnMaximumValue() {
+        Assert.assertEquals(12.1f, MaximumOfGivenNumbers.checkMaximum(12.1f, 10.3f, 3.2f,1.1f,2.5f,4.8f),0.0f);
+    }
+
+    @Test
+    public void givenMultipleStrings_ShouldReturnMaximumValue() {
+        Assert.assertEquals("Strawberry", MaximumOfGivenNumbers.checkMaximum("Peach","Apple","Banana","Mango","Pineapple","Strawberry"));
     }
 }
